d666f36f376a93188cf81b165395900ea1883915
Anant Tripathi <ianantktripathi@gmail.com>
2024-05-17 21:58:29 +0530

    Sorted 2 SUM with no duplicates

---
+++ src/main/java/anant/practice/D160524/TwoSumSortedAndNoDuplicates.java
@@ -0,0 +1,32 @@
+package anant.practice.D160524;
+
+public class TwoSumSortedAndNoDuplicates {
+    public static void main(String[] args) {
+        int []nums={-1,0,0,1,2,2,7};
+        int target=1;
+        int i=0;
+        int j=nums.length-1;
+        int sum=0;
+        while(i<j){
+            sum=nums[i]+nums[j];
+            if(sum>target){
+                j--;
+            }
+            else if (sum<target) {
+                i++;
+            }
+            else {
+                if(nums[i]==nums[i+1]){
+                    i++;
+                    continue;
+                } else if (nums[j]==nums[j-1]) {
+                    j--;
+                    continue;
+                }
+                System.out.println(i +" "+j);
+                j--;
+                i++;
+            }
+        }
+    }
+}
